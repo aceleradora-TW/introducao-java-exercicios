c1a8795027fdf9b80a786968908534854e597116
yrachid <anuar.yasser@gmail.com>
2020-05-03 18:31:11 -0300

    @yrachid: Mais exercicios

--- src/main/java/com/agil/aceleradora/exercicios/Calculos.java
+++ src/main/java/com/agil/aceleradora/exercicios/Calculos.java
@@ -4,7 +4,7 @@ package com.agil.aceleradora.exercicios;
 
 Implemente os metodos Abaixoo cumprindo cada um dos requisitos.
 
-Crie um metodo main para testar cada um deles. Preste atencao
+Teste os metodos no metodo main abaixo. Preste atencao
 em quais deles sao estaticos e quais nao sao, lembre-se que
 os metodos que nao sao estaticos necessitam ser chamados
 atraves de um objeto.
@@ -13,24 +13,43 @@ public class Calculos {
 
     // Implemente a soma de a + b
     public static int soma(int a, int b) {
-        throw new RuntimeException("Nao fui implementado ainda");
+        return -1;
     }
 
     // Implemente a subtracao de a - b
     public int diferenca(int a, int b) {
-        throw new RuntimeException("Nao fui implementado ainda");
+        return -1;
     }
 
     // Implemente a media aritmetica a, b, c
     // Formula: (a + b + c) / 3
     public float media(int a, int b, int c) {
-        throw new RuntimeException("Nao fui implementado ainda");
+        return -1;
     }
 
     // Distancia entre a e b
     // Distancia eh o resultado da subtracao sem sinal
     // Lembre-se do metodo Math.abs()
     public int distancia(int a, int b) {
-        throw new RuntimeException("Nao fui implementado ainda");
+        return -1;
+    }
+
+    // Retorna o maior entre a e b
+    // Formula: (a + b + Math.abs(a - b)) / 2
+    // Bonus: Pesquise um metodo da classe Math que faca o calculo acima.
+    public static int maximo(int a, int b) {
+        return -1;
+    }
+
+    // Retorna o menor entre a e b
+    // Formula: (a + b - Math.abs(a - b)) / 2
+    // Bonus: Pesquise um metodo da classe Math que faca o calculo acima.
+    public static int minimo(int a, int b) {
+        return -1;
+    }
+
+    // Teste os metodos aqui:
+    public static void main(String[] args) {
+
     }
 }
