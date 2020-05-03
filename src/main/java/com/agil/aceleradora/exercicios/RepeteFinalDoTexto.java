7ce78628fe233212aa1bcca2bab1aff0a911ffec
yrachid <anuar.yasser@gmail.com>
2020-05-03 18:57:46 -0300

    @yrachid: RepeteFinalDoTexto

---
+++ src/main/java/com/agil/aceleradora/exercicios/RepeteFinalDoTexto.java
@@ -0,0 +1,23 @@
+package com.agil.aceleradora.exercicios;
+
+/*
+Implemente o método repetir, que recebe como argumento uma String e um número inteiro.
+O método deve retornar uma nova String contendo a junção dos n últimos caracteres da string recebida repetidos n vezes,
+onde n é o número passado como argumento para o método.
+
+Exemplos de entrada e saida:
+
+repetir("teste", 2); // retorna "tete"
+
+repetir("coca-cola", 3); // retorna "olaolaola"
+
+repetir("oi", 0); // retorna ""
+
+repetir("pepsi", 5); // retorna "pepsipepsipepsipepsipepsi"
+ */
+public class RepeteFinalDoTexto {
+
+    public static String repetir(String texto, int repeticoes) {
+        return "";
+    }
+}
