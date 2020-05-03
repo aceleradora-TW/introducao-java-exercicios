9b7e0b86e9b2aa6b8765cbd178d44dccfbbebbb4
yrachid <anuar.yasser@gmail.com>
2020-05-03 19:07:04 -0300

    @yrachid: Inverter numero

---
+++ src/main/java/com/agil/aceleradora/exercicios/InverteNumero.java
@@ -0,0 +1,26 @@
+package com.agil.aceleradora.exercicios;
+
+/*
+
+O metodo inverter recebe um inteiro. Ele deve retornar outro inteiro
+com os algarismos invertidos. Por exemplo:
+
+inverter(12) // retorna 21
+inverter(1) // retorna 1
+inverter(1234) // retorna 4321
+
+Dicas:
+
+Transformar inteiro em String:
+Integer.toString() transforma um inteiro em uma String
+
+Transformar String em vetor:
+String valor = "abc";
+String[] separado = valor.split(""); // separado == ["a", "b", "c"]
+ */
+public class InverteNumero {
+
+    public int inverter(int valor) {
+        return valor;
+    }
+}
