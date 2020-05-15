145f77e7349b7f64cac612dd22520c8ccc88d03e
yrachid <anuar.yasser@gmail.com>
2020-05-15 12:15:32 -0300

    Solucao: RepeteFinalDoTexto

--- src/main/java/com/agil/aceleradora/exercicios/RepeteFinalDoTexto.java
+++ src/main/java/com/agil/aceleradora/exercicios/RepeteFinalDoTexto.java
@@ -18,6 +18,37 @@ repetir("pepsi", 5); // retorna "pepsipepsipepsipepsipepsi"
 public class RepeteFinalDoTexto {
 
     public static String repetir(String texto, int repeticoes) {
-        return "";
+        return repeticoes == 0
+                ? ""
+                : texto
+                .substring(texto.length() - repeticoes)
+                .repeat(repeticoes);
+    }
+
+    public static String repetirManual(String texto, int repeticoes) {
+        if (repeticoes == 0) {
+            return "";
+        }
+
+        String pedaco = texto.substring(texto.length() - repeticoes);
+
+        String resultado = "";
+        for (int i = 0; i < repeticoes; i++) {
+            resultado += pedaco;
+        }
+
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(repetir("teste", 2));
+        System.out.println(repetir("coca-cola", 3));
+        System.out.println(repetir("oi", 0));
+        System.out.println(repetir("pepsi", 5));
+        System.out.println("-------------------------");
+        System.out.println(repetirManual("teste", 2));
+        System.out.println(repetirManual("coca-cola", 3));
+        System.out.println(repetirManual("oi", 0));
+        System.out.println(repetirManual("pepsi", 5));
     }
 }
