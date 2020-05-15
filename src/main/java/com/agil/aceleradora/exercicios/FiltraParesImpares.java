bd2916e9d19e06f6181ea8b08a875b728bee2795
yrachid <anuar.yasser@gmail.com>
2020-05-15 11:57:54 -0300

    Solucao: FiltraParesImpares com copia

--- src/main/java/com/agil/aceleradora/exercicios/FiltraParesImpares.java
+++ src/main/java/com/agil/aceleradora/exercicios/FiltraParesImpares.java
@@ -8,6 +8,15 @@ public class FiltraParesImpares {
         return numero % 2 == 0;
     }
 
+    private static int[] expandeArray(int[] original) {
+        return Arrays.copyOf(original, original.length + 1);
+    }
+
+    private static boolean arrayEstaCheio(int ocupacao, int[] array) {
+        return array.length == ocupacao;
+    }
+
+
     /*
     Este metodo recebe um vetor de inteiros como parametros e
     deve retornar um novo vetor contendo somente os numeros
@@ -72,12 +81,34 @@ public class FiltraParesImpares {
         return impares;
     }
 
+    // Outra forma de resolver com apenas um loop
+    private static int[] filtraParesComCopia(int[] numeros) {
+        int[] pares = new int[1];
+        int ocupacao = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            boolean ehUltimaIteracao = i == numeros.length - 1;
+
+            if (ehPar(numeros[i])) {
+                pares[ocupacao] = numeros[i];
+                ocupacao++;
+                if (arrayEstaCheio(ocupacao, pares) && !ehUltimaIteracao) {
+                    pares = expandeArray(pares);
+                }
+            }
+        }
+
+        return pares;
+    }
+
     // Dica: Utilize Arrays.toString(vetor) para imprimir um vetor de maneira legivel
     public static void main(String[] args) {
         int[] pares = filtraPares(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+        int[] pares2 = filtraParesComCopia(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         int[] impares = filtraImpares(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 
         System.out.println(Arrays.toString(pares));
+        System.out.println(Arrays.toString(pares2));
         System.out.println(Arrays.toString(impares));
     }
 }
